6425484649cda691fdea526a5ed7e6d859fbbc9d
manuelechavarria <maechavarriaor@gmail.com>
2026-08-06 08:19:21 -0500

    video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -30,7 +30,7 @@
       .col-lg-12
         figure(data-aos="fade-down")
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/DC1CjF8waro?si=KnKx32zBBlHGeM2k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
