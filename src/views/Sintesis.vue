d62e5ef18e2a5772853574ed408e93ff94ad5c09
manuelechavarria <maechavarriaor@gmail.com>
2026-09-02 17:23:03 -0500

    sintesis

--- src/views/Sintesis.vue
+++ src/views/Sintesis.vue
@@ -7,7 +7,7 @@
 
     p.mt-4(data-aos="fade-down") De igual forma, se promueve una cultura de prevención orientada a la reducción de riesgos y a la actuación responsable frente a eventos adversos, así como la adecuada activación del sistema de emergencias. Todo ello con el propósito de garantizar una atención articulada, segura y centrada en la protección integral de la vida, la salud y el bienestar de niñas y niños en los diferentes contextos de cuidado.
 
-    p.mt-4.mb-0(data-aos="fade-down") A continuación, el mapa conceptual organiza de manera visual los ejes temáticos del componente formativo y las relaciones que los articulan.
+    p.mt-4.mb-0(data-aos="fade-down") A continuación, el mapa conceptual organiza de manera estructurada los ejes temáticos del componente formativo y las relaciones que los articulan.
 
     figure.mt-4(data-aos="fade-down")
       img(src="@/assets/curso/sintesis.svg" alt="El mapa conceptual &quot;Principios de primeros auxilios en primera infancia&quot; organiza los contenidos del componente en seis ejes: generalidades, marco normativo y ético, fundamentos conceptuales y gestión del riesgo, bioseguridad y rol del primer respondiente, botiquín, y evaluación del escenario y toma de decisiones, con sus subtemas. Así, evidencia que la atención inicial es un proceso integral orientado a proteger la vida e integridad de niñas y niños en Colombia. " style="width: 1228px").m-auto
